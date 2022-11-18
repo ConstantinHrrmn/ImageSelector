1e363342a56d5d8ac39539e46fa47a8c757af56e
ConstantinHrrmn <31920335+ConstantinHrrmn@users.noreply.github.com>
2022-11-18 14:04:40 +0100

    Adding the FTP service to communicate directly with the server

--- WaviewPhotoSelector/WaviewPhotoSelector/Program.cs
+++ WaviewPhotoSelector/WaviewPhotoSelector/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System.Drawing;
+using System.Net;
+using WaviewPhotoSelector;
 
 #region Variables d'affichage
 
@@ -6,9 +8,10 @@
 
 string MAIN_MENU_STRING = 
     "[BEFORE SENDING] : Check link between RAW and JPG, rename files and prepare folders to send. " +
+    "\n[BEFORE SENDING + UPLOAD] : Check link between RAW and JPG, rename files and prepare folders to send. Also upload thumbnails/photos to server." +
     "\n[FIND FILES MESSAGE] : Get all selected photos from a list of photos. Recieved by message" +
     "\n[FIND FILES SERVER] : Get all selected photos on the server"+
-    "\n\nEnter your choice:\n\n-Before sending [b]\n-Find files from message [f]\n-Find files from server [s]\n\nChoice [b / f / s]: ";
+    "\n\nEnter your choice:\n\n-Before sending [b]\n-Before sending + upload [u]\n-Find files from message [f]\n-Find files from server [s]\n\nChoice [b / u / f / s]: ";
 
 string END_STRING = 
     "DONE. Restart [r] or Quit [q] : ";
@@ -19,14 +22,17 @@ string WRONG_INPUT =
 string LIST_INPUT =
     "Please paste list of image numbers to find separated by ','. (ex : 1, 3, 6) : ";
 
+string OUTPUT_FILE = "z_output.txt";
+string URL = "http://www.waview.ch/akesso/gallery/";
+
 #endregion
 
-
+//WavFTP.UploadFile(null, "/test/test");
 
 // Toutes les actions possibles par l'utilisateur.
 // Ne pas oublier d'ajouter l'action dans la fonction "MakeAction" afin de pouvoir l'utiliser.
 // Il faut également créer la méthode correspondante.
-char[] CHOICES = { 'b', 'f' };
+char[] CHOICES = { 'b', 'u', 'f', 's' };
 
 do
 {
@@ -87,21 +93,145 @@ void MakeAction(string input)
     switch (input.ToLower())
     {
         case "b":
-            B();
-            //BeforeSending();
+            B(false);
             break;
         case "f":
-            FindFiles();
+            FindFiles(null);
             break;
         case "s":
-            FindFiles();
+            S();
+            break;
+        case "u":
+            B(true);
             break;
         default:
             break;
     }
 }
 
-void B()
+void S()
+{
+    string choosen = GetPathOnServer();
+    if (IsThereValidList(choosen))
+    {
+        ChangeConsoleColor(ConsoleColor.Green);
+        Console.WriteLine("List found !");
+
+        WebClient wc = new WebClient();
+        string theTextFile = wc.DownloadString(URL + choosen + "/" + OUTPUT_FILE);
+
+        //Console.WriteLine(theTextFile);
+        GetFilesFromServer(theTextFile);
+    }
+    else
+    {
+        ChangeConsoleColor(ConsoleColor.Red);
+        Console.WriteLine("List not found...");
+    }
+}
+
+bool IsThereValidList(string path)
+{
+    List<string> files = WavFTP.listFiles("/"+path, false);
+    files.Reverse();
+
+    return files.Contains(OUTPUT_FILE);
+}
+
+string GetPathOnServer()
+{
+    int index = 1;    
+    List<string> places = WavFTP.listFiles("", true);
+
+    ChangeConsoleColor(ConsoleColor.Green);
+
+    foreach (string place in places)
+    {
+        Console.WriteLine("["+index+"] " + place);
+        index++;
+    }
+
+    // CHOIX DE L'ENDROIT
+    bool ok = false;
+    int choiceInt = -1;
+    do
+    {
+        try
+        {
+            ChangeConsoleColor(ConsoleColor.White);
+            Console.Write("Enter index of place to get photos from : ");
+            string choice = ReadLine();
+            choiceInt = int.Parse(choice);
+            ok = true;
+
+            if (choiceInt < 0 || choiceInt > places.Count)
+            {
+                ok = false;
+                ChangeConsoleColor(ConsoleColor.Red);
+                Console.WriteLine("Invalid index...");
+            }
+
+        }
+        catch (Exception)
+        {
+            ChangeConsoleColor(ConsoleColor.Red);
+            Console.WriteLine("Wrong input... Please try again");
+            ok = false;
+        }
+    } while (!ok);
+
+    string choosen_place = places[choiceInt-1];
+
+    // CHOIX DE L'ALBUM PHOTO
+    List<string> albums = WavFTP.listFiles("/"+choosen_place, true);
+    index = 1;
+    
+    ChangeConsoleColor(ConsoleColor.Green);
+
+    foreach (string album in albums)
+    {
+        Console.WriteLine("[" + index + "] " + album);
+        index++;
+    }
+
+    ok = false;
+    choiceInt = -1;
+    do
+    {
+        try
+        {
+            ChangeConsoleColor(ConsoleColor.White);
+            Console.Write("Enter index of the album to get photos from : ");
+            string choice = ReadLine();
+            choiceInt = int.Parse(choice);
+            ok = true;
+
+            if (choiceInt < 0 || choiceInt > places.Count)
+            {
+                ok = false;
+                ChangeConsoleColor(ConsoleColor.Red);
+                Console.WriteLine("Invalid index...");
+            }
+
+        }
+        catch (Exception)
+        {
+            ChangeConsoleColor(ConsoleColor.Red);
+            Console.WriteLine("Wrong input... Please try again");
+            ok = false;
+        }
+    } while (!ok);
+
+    string choosen_album = albums[choiceInt - 1];
+    string path = choosen_place + "/" + choosen_album + "/";
+    
+    ChangeConsoleColor(ConsoleColor.Blue);
+    Console.WriteLine("Choosen album : " + path);
+    ChangeConsoleColor(ConsoleColor.White);
+    return path;
+}
+
+void B(bool upload)
 {
     string main_path = "";
 
@@ -117,9 +247,6 @@ void B()
         string thumbnail_path = Path.Combine(main_path, "thumbnails");
         double ratio = 0;
 
-        if (!Directory.Exists(thumbnail_path))
-            Directory.CreateDirectory(thumbnail_path);
-
         ChangeConsoleColor(ConsoleColor.Green);
         Console.WriteLine("Directory " + main_path + " is OK.");
         ChangeConsoleColor(ConsoleColor.White);
@@ -129,6 +256,9 @@ void B()
 
         if (choice == "y")
         {
+            if (!Directory.Exists(thumbnail_path))
+                Directory.CreateDirectory(thumbnail_path);
+            
             if (thumbnail_path != null)
             {
                 Console.Write("Enter the ratio of the thumbnails (0 to 1) : ");
@@ -147,23 +277,30 @@ void B()
 
         Console.Clear();
 
+        if (upload)
+        {
+            
+        }
+
+        string dir_if_needed = "";
+        List<List<string>> all_files = new List<List<string>>();
+
         foreach (string directory  in directories)
         {
-            if (directory == "thumbnails")
+            if (directory.Contains("thumbnails"))
                 break;
 
             string jpg_path = Path.Combine(main_path, directory, "JPG");
             string raw_path = Path.Combine(main_path, directory, "RAW");
             string main_camera_path = Path.Combine(main_path, directory);
+            dir_if_needed = jpg_path;
 
             if (Directory.Exists(jpg_path) && Directory.Exists(raw_path))
             {
-                
-
                 if (choice == "y")
-                    BeforeSending(main_camera_path, jpg_path, raw_path, thumbnail_path, ratio);
+                    all_files.Add(BeforeSending(main_camera_path, jpg_path, raw_path, thumbnail_path, ratio, upload));
                 else
-                    BeforeSending(main_camera_path, jpg_path, raw_path, null, 0);
+                    all_files.Add(BeforeSending(main_camera_path, jpg_path, raw_path, null, 0, upload));
             }
             else
             {
@@ -173,10 +310,31 @@ void B()
             }
         }
 
-        string path_to_file = Path.GetFullPath(Directory.GetFiles(thumbnail_path)[0]);
-        string file_name = Path.GetFileName(path_to_file);
+        List<string> cover = new List<string>();
+        if (choice == "y")
+        {
+            string path_to_file = Path.GetFullPath(Directory.GetFiles(thumbnail_path)[0]);
+            string file_name = Path.GetFileName(path_to_file);
+            File.Copy(path_to_file, path_to_file.Replace(file_name, "cover.JPG"), true);
+            cover.Add(path_to_file.Replace(file_name, "cover.JPG"));
+        }
+        else
+        {
+            string path_to_file = Path.GetFullPath(Directory.GetFiles(dir_if_needed)[0]);
+            string file_name = Path.GetFileName(path_to_file);
+            File.Copy(path_to_file, Path.Combine(main_path, "cover.JPG"), true);
+            cover.Add(Path.Combine(main_path, "cover.JPG"));
+        }
         
-        File.Copy(path_to_file, path_to_file.Replace(file_name, "cover.JPG"), true);
+        all_files.Add(cover);
+        if (upload)
+        {         
+            WavFTP.UploadFile(all_files, CreateRemoteFolder());
+            ChangeConsoleColor(ConsoleColor.Green);
+            Console.WriteLine("Upload finished !");
+            ChangeConsoleColor(ConsoleColor.White);
+        }
+        
     }
     else
     {
@@ -200,12 +358,14 @@ void ChangeConsoleColor(ConsoleColor color)
 /// Si les fichiers correspondent, alors on les renomme en fonction de la caméra. 
 /// Sinon, les fichiers RAW sans JPG et les fichiers JPG sans fichiers RAW sont placés dans des dossier "poubelle"
 /// </summary>
-void BeforeSending(string main_path, string jpg_path, string raw_path, string thumbnail_path, double ratio)
+List<string> BeforeSending(string main_path, string jpg_path, string raw_path, string thumbnail_path, double ratio, bool upload)
 {
     
     string jpg_trash_path = "NO_RAW_MATCH";
     string raw_trash_path = "NO_JPG_MATCH";
     int counter = 1;
+
+    List<string> FilesToSend = new List<string>();
 
 
     // On vérifie si le chemin existe
@@ -234,11 +394,12 @@ void BeforeSending(string main_path, string jpg_path, string raw_path, string th
         // On parcours toutes les images dans le dossier JPG
         foreach (string jpg_file in JPG_FILES)
         {
-            string[] photo_name_parts = Path.GetFileNameWithoutExtension(jpg_file).Split('_');
-            string code = photo_name_parts[photo_name_parts.Length - 1];
+            //string[] photo_name_parts = Path.GetFileNameWithoutExtension(jpg_file).Split('_');
+            //string code = photo_name_parts[photo_name_parts.Length - 1];
+            string code = Path.GetFileNameWithoutExtension(jpg_file);
 
             // On cherche le fichier RAW correspondant
-            string raw_file = RAW_FILES.FirstOrDefault(x => Path.GetFileNameWithoutExtension(x).Contains(code));
+            string raw_file = RAW_FILES.FirstOrDefault(x => Path.GetFileNameWithoutExtension(x).Equals(code), null);
             
             if (raw_file == null)
             {
@@ -255,12 +416,38 @@ void BeforeSending(string main_path, string jpg_path, string raw_path, string th
                 string new_jpg_path = Path.Combine(jpg_path, camera_name + "_" + counter + Path.GetExtension(jpg_file));
                 string new_raw_path = Path.Combine(raw_path, camera_name + "_" + counter + Path.GetExtension(raw_file));
 
-                File.Move(jpg_file, new_jpg_path, true);
-                File.Move(raw_file, new_raw_path, true);
+                try
+                {
+                    File.Move(jpg_file, new_jpg_path, true);
+                }
+                catch (Exception)
+                {
+                    ChangeConsoleColor(ConsoleColor.Red);
+                    Console.WriteLine("ERROR WITH JPG FILE : " + Path.GetFileName(jpg_file));
+                    ChangeConsoleColor(ConsoleColor.White);
+                }
+
+                try
+                {
+                    File.Move(raw_file, new_raw_path, true);
+                }
+                catch (Exception)
+                {
+                    ChangeConsoleColor(ConsoleColor.Red);
+                    Console.WriteLine("ERROR WITH RAW FILE : " + Path.GetFileName(raw_file));
+                    ChangeConsoleColor(ConsoleColor.White);
+                }
+                
+                
 
                 if (thumbnail_path != null)
-                    CreateThumbnail(new_jpg_path, thumbnail_path, ratio, null);
-
+                {
+                    FilesToSend.Add(CreateThumbnail(new_jpg_path, thumbnail_path, ratio, null));
+                }
+                else
+                {
+                    FilesToSend.Add(new_jpg_path);
+                }
                 // On incrémente le compteur
                 counter++;
             }
@@ -297,20 +484,120 @@ void BeforeSending(string main_path, string jpg_path, string raw_path, string th
         Console.WriteLine("The path doesn't exist. Please try again. Press ENTER");
         Console.ReadKey();
     }
+
+    return FilesToSend;
+}
+
+string CreateRemoteFolder()
+{
+    int index = 1;
+    List<string> places = WavFTP.listFiles("", true);
+
+    ChangeConsoleColor(ConsoleColor.Green);
+
+    foreach (string place in places)
+    {
+        Console.WriteLine("[" + index + "] " + place);
+        index++;
+    }
+
+    Console.WriteLine("[" + index + "] New folder");
+
+    // CHOIX DE L'ENDROIT
+    bool ok = false;
+    int choiceInt = -1;
+    do
+    {
+        try
+        {
+            ChangeConsoleColor(ConsoleColor.White);
+            Console.Write("Enter index of place to get photos from : ");
+            string choice = ReadLine();
+            choiceInt = int.Parse(choice);
+            ok = true;
+
+            if (choiceInt < 0 || choiceInt > places.Count+1)
+            {
+                ok = false;
+                ChangeConsoleColor(ConsoleColor.Red);
+                Console.WriteLine("Invalid index...");
+            }
+
+        }
+        catch (Exception)
+        {
+            ChangeConsoleColor(ConsoleColor.Red);
+            Console.WriteLine("Wrong input... Please try again");
+            ok = false;
+        }
+    } while (!ok);
+
+    string placeName = "";
+    
+    if (choiceInt > places.Count)
+    {
+        Console.Write("Enter name of new folder : ");
+        placeName = ReadLine();
+
+        Console.Write("Enter the name of the place : ");
+        string place = ReadLine();
+
+        string fileName = @"C:\Users\Constantin\Documents\folder_name.txt";
+
+        // Check if file already exists. If yes, delete it.     
+        if (File.Exists(fileName))
+        {
+            File.Delete(fileName);
+        }
+
+        // Create a new file     
+        using (StreamWriter sw = File.CreateText(fileName))
+        {
+            sw.WriteLine(place);
+        }
+
+        string f = WavFTP.CreateFolder(placeName);
+        WavFTP.UploadFile(fileName, f, "folder_name.txt");
+
+    }
+    else
+    {
+        placeName = places[choiceInt - 1];
+    }
+    
+
+    Console.Write("Enter name of the folder to create : ");
+    string folderName = ReadLine();
+
+    string path = WavFTP.CreateFolder(placeName + "/" + folderName);
+    string folder = WavFTP.CreateFolder(placeName);
+
+    if (choiceInt > places.Count)
+    {
+        Console.Write("Path to place cover : ");
+        string cover = ReadLine();
+        WavFTP.UploadFile(cover, folder, "cover.jpg");
+    }
+
+    return placeName + "/" + folderName;
 }
 
 /// <summary>
 /// Create a thumbnail image in the thumbnail folder of a % of the size of the original image
 /// </summary>
-void CreateThumbnail(string photo_path, string thumbnail_path, double ratio, string new_name)
+string CreateThumbnail(string photo_path, string thumbnail_path, double ratio, string new_name)
 {
     if (File.Exists(photo_path))
     {
         if (Directory.Exists(thumbnail_path))
         {
-            ResizeImageOnRatio(Image.FromFile(photo_path), ratio).Save(Path.Combine(thumbnail_path, (new_name != null ? new_name : Path.GetFileName(photo_path))));
+            string path = Path.Combine(thumbnail_path, (new_name != null ? new_name : Path.GetFileName(photo_path)));
+            ResizeImageOnRatio(Image.FromFile(photo_path), ratio).Save(path);
+            return path;
         }
     }
+
+    return null;
 }
 
 Bitmap ResizeImageOnRatio(Image source_image, double ratio)
@@ -333,7 +620,7 @@ Bitmap ResizeImageOnRatio(Image source_image, double ratio)
 /// <summary>
 /// On parcours le dossier raw à la recherche de toutes les images dans la liste
 /// </summary>
-void FindFiles()
+void FindFiles(string list)
 {
     string main_path = "";
     string raw_path = "";
@@ -369,7 +656,16 @@ void FindFiles()
         DeleteDSStore(raw_path);
 
         Console.Write(LIST_INPUT);
-        string index_list = ReadLine();
+        string index_list;
+
+        if (list == null)
+        {
+            index_list = ReadLine();
+        }
+        else
+        {
+            index_list = list;
+        }
 
         string[] splitted_indexes = index_list.Split(',');
         List<string> indexes = new List<string>();
@@ -439,13 +735,65 @@ void FindFiles()
         Console.WriteLine("The path doesn't exist. Please try again. Press ENTER");
         Console.ReadKey();
         // On relance la méthode
-        FindFiles();
+        FindFiles(null);
     }
 }
 
-void FindFilesServer(string main_path, string text_list_path)
+void GetFilesFromServer(string myList)
 {
-    
+    string main_path = "";
+    string selected_path = "";
+    string selected_name = "SELECTED";
+    int cpt = 0;
+
+    string[] list = myList.Split(",");
+    List<string> indexes = new List<string>();
+
+    foreach (string l in list)
+        indexes.Add(l.Split(".")[0]);
+
+    Console.Clear();
+
+    ChangeConsoleColor(ConsoleColor.White);
+    // On demande à l'utilisateur de rentrer le chemin du dossier parent avec les photos
+    Console.Write("Enter the path of the main folder (images) : ");
+    main_path = ReadLine();
+
+    selected_path = Path.Combine(main_path, selected_name);
+
+    if (!Directory.Exists(selected_path))
+        Directory.CreateDirectory(selected_path);
+
+    string[] directories = Directory.GetDirectories(main_path);
+
+    ChangeConsoleColor(ConsoleColor.Yellow);
+    foreach (string dir in directories)
+    {
+        if (dir.Contains("thumbnails") || dir.Contains(selected_name))
+            break;
+
+        string raw_path = Path.Combine(dir, "RAW");
+        if (Directory.Exists(raw_path))
+        {
+            string[] files = Directory.GetFiles(raw_path);
+            foreach (string file in files)
+            {
+                string fileName = Path.GetFileNameWithoutExtension(file);
+                foreach (string index in indexes)
+                {
+                    if (fileName == index)
+                    {
+                        Console.WriteLine("MATCH : " + fileName);
+                        File.Copy(file, Path.Combine(selected_path, Path.GetFileName(file)));
+                        cpt++;
+                    }
+                }
+            }
+        }
+    }
+    ChangeConsoleColor(ConsoleColor.Green);
+    Console.WriteLine("Found : " + cpt + "/" + indexes.Count);
+    ChangeConsoleColor(ConsoleColor.White);
 }
 
 /// <summary>
@@ -460,3 +808,4 @@ void DeleteDSStore(string path)
             File.Delete(file);
     }
 }
+
---
+++ WaviewPhotoSelector/WaviewPhotoSelector/WavFTP.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Renci.SshNet;
+using Renci.SshNet.Common;
+using Renci.SshNet.Sftp;
+
+namespace WaviewPhotoSelector
+{
+    class WavFTP
+    {
+        static string host = @"ssh.waview.ch";
+        static string username = "waview.ch";
+        static string password = @"ssh_waview_!";
+        static string remoteDirectory = "/customers/3/8/4/waview.ch/httpd.www/akesso/gallery/";
+
+        public static List<string> listFiles(string path, bool directory)
+        {
+            List<string> folders = new List<string>();
+            using (SftpClient sftp = new SftpClient(host, username, password))
+            {
+                try
+                {
+                    sftp.Connect();
+
+
+                    var files = sftp.ListDirectory(remoteDirectory + path);
+
+                    foreach (var file in files)
+                    {
+                        if (file.Name != "." && file.Name != "..")
+                        {
+                            if (directory)
+                            {
+                                if (file.IsDirectory)
+                                {
+                                    folders.Add(file.Name);
+                                }
+                            }
+                            else
+                            {
+                                folders.Add(file.Name);
+                            }
+                        }
+
+                    }
+
+                    sftp.Disconnect();
+                    return folders;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("An exception has been caught " + e.ToString());
+                    return null;
+                }
+            }
+        }
+
+        public static void UploadFile(List<List<string>> all_files, string path)
+        {
+            if (!path.Contains(path))
+                path = remoteDirectory + path;
+
+            Console.WriteLine(path);
+
+            using (SftpClient sftp = new SftpClient(host, username, password))
+            {
+                try
+                {
+                    sftp.Connect();
+                    
+                    sftp.ChangeDirectory(CreateFolder(path));
+
+                    foreach (List<string> item in all_files)
+                    {
+                        foreach (string i in item)
+                        {
+                            Console.WriteLine(i);
+                            using (FileStream fs = new FileStream(i, FileMode.Open))
+
+                            {
+                                sftp.BufferSize = 4 * 1024;
+
+                                sftp.UploadFile(fs, Path.GetFileName(i));
+                            }
+                        }
+                    }
+
+                    sftp.Disconnect();
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("An exception has been caught " + e.ToString());
+                }
+
+            }
+
+        }
+
+        public static void UploadFile(string file, string path, string name)
+        {
+            Console.WriteLine("Path for cover : " + path);
+            if (!path.Contains(path))
+                path = remoteDirectory + path;
+
+            using (SftpClient sftp = new SftpClient(host, username, password))
+            {
+                try
+                {
+                    sftp.Connect();
+                    
+                    sftp.ChangeDirectory(path);
+
+                    using (FileStream fs = new FileStream(file, FileMode.Open))
+
+                    {
+                        sftp.BufferSize = 4 * 1024;
+
+                        sftp.UploadFile(fs, name);
+                    }
+
+                    sftp.Disconnect();
+                    Console.WriteLine("Upload complete.");
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("An exception has been caught " + e.ToString());
+                }
+            }
+        }
+
+        public static string CreateFolder(string path)
+        {
+            path = remoteDirectory + path;
+
+            using (SftpClient sftp = new SftpClient(host, username, password))
+            {
+                try
+                {
+                    sftp.Connect();
+
+                    string current = "";
+
+                    if (path[0] == '/')
+                    {
+                        path = path.Substring(1);
+                    }
+
+                    while (!string.IsNullOrEmpty(path))
+                    {
+                        int p = path.IndexOf('/');
+                        current += '/';
+                        
+                        if (p >= 0)
+                        {
+                            current += path.Substring(0, p);
+                            path = path.Substring(p + 1);
+                        }
+                        else
+                        {
+                            current += path;
+                            path = "";
+                        }
+
+                        try
+                        {
+                            SftpFileAttributes attrs = sftp.GetAttributes(current);
+                            if (!attrs.IsDirectory)
+                            {
+                                throw new Exception("not directory");
+                            }
+                        }
+                        catch (SftpPathNotFoundException)
+                        {
+                            sftp.CreateDirectory(current);
+                        }
+                    }
+
+                    sftp.Disconnect();
+
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine(current);
+                    Console.ForegroundColor = ConsoleColor.White;
+                    return current;
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("An exception has been caught " + e.ToString());
+                }
+
+                return null;
+            }
+        }
+    }
+}
